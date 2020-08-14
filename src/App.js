222bdc77ba6bb6ba49f0420ba998bcf5ccd2c52a
graceFor <tbgusrud12@gmail.com>
2020-08-15 02:44:30 +0900

    [make Component]

--- src/App.js
+++ src/App.js
@@ -3,29 +3,56 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-/*function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>WEB</h1>
+        world wide web!
       </header>
-    </div>
-  );
-}*/
+    );
+  }
+}
+class Nav extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a href="1.html">HTML</a>
+          </li>
+          <li>
+            <a href="2.html">CSS</a>
+          </li>
+          <li>
+            <a href="3.html">JavaScript</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML is HyperText Markup Language.
+      </article>
+    );
+  }
+}
+
 class App extends Component {
   render() {
-    return <div className="App">Hello, React!!</div>;
+    return (
+      <div className="App">
+        <Subject></Subject>
+        <Nav></Nav>
+        <Content></Content>
+      </div>
+    );
   }
 }
 export default App;

b9c819be83991f3f69176b3c2f53d2a41bd7d378
graceFor <tbgusrud12@gmail.com>
2020-08-14 23:17:40 +0900

    [update App.js]

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import React from 'react';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +22,10 @@ function App() {
       </header>
     </div>
   );
+}*/
+class App extends Component {
+  render() {
+    return <div className="App">Hello, React!!</div>;
+  }
 }
-
 export default App;

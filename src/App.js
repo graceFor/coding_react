50a89bec85fb77ca9840f6476d4e68793430bf3c
graceFor <tbgusrud12@gmail.com>
2020-08-15 02:51:29 +0900

    [props]

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ class Subject extends Component {
   render() {
     return (
       <header>
-        <h1>WEB</h1>
-        world wide web!
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
       </header>
     );
   }
@@ -48,7 +48,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Subject></Subject>
+        <Subject title="WEB" sub="world wide web!"></Subject>
         <Nav></Nav>
         <Content></Content>
       </div>
